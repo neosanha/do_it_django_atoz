c1da89a65b25fc241877c019f723e95e2978d6eb
sanha <sanha@naver.com>
2022-09-15 09:24:11 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 import os
 
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -13,10 +13,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
