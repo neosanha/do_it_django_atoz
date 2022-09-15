109c87332fc0d8e6e6260abe17789f929135b513
sanha <sanha@naver.com>
2022-09-16 07:22:09 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class PostList(ListView):
@@ -29,7 +29,7 @@ class PostDetail(DetailView):
 
 def category_page(request, slug):
     if slug == 'no_category':
-        category = 'unclassified'
+        category = '미분류'
         post_list = Post.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
@@ -43,5 +43,21 @@ def category_page(request, slug):
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html/',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
