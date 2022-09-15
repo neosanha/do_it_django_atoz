b07fddc00b71529a36f543b870cf00cb149c347a
sanha <sanha@naver.com>
2022-09-15 11:52:43 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,20 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
-from .models import Post
+from .models import Post, Category
 
-
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#     return render(request, 'blog/index.html', {'posts': posts})
 
 class PostList(ListView):
     model = Post
     # template_name = 'blog/index.html'
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-#     return render(request, 'blog/single_post_page.html', {'post': post})
 
 class PostDetail(DetailView):
     model = Post
