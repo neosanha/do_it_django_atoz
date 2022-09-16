1c1c4fa94c61edbbd0ded05e7a7d68dd80ce2e70
sanha <sanha@naver.com>
2022-09-16 09:16:47 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.index),
-    # path('<int:pk>/', views.single_post_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
 ]
