9a21cb486d26f28dd218dbb2be536d1cb900b5f9
sanha <sanha@naver.com>
2022-09-19 12:46:54 +0900

    댓글 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/new_comment/', views.new_comment),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('', views.PostList.as_view()),
 ]
