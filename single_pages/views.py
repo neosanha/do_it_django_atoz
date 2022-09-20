18963190d9195303660f41da394c1f74e2341fc9
sanha <sanha@naver.com>
2022-09-20 12:00:43 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def landing(request):
-    return render(request, 'single_pages/landing.html', {})
+    recent_posts = Post.objects.order_by('-pk')[:3]
+
+    return render(request, 'single_pages/landing.html', {'recent_posts': recent_posts})
 
 
 def about_me(request):
