248d64d209289cdb7c0859b8814e0668450330fc
sanha <sanha@naver.com>
2022-09-22 13:38:00 +0900

    도커에 Gunicorn 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -143,9 +143,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
